db8257a759908679c8e6b08cf31b1b7590e707eb
Kelvin Capitan Casasola <47744014+Kelvin-Capitan@users.noreply.github.com>
2020-12-02 23:00:14 -0300

    v 0.4
    v 0.4

---
+++ App.js
@@ -0,0 +1,73 @@
+import { Text, View, StyleSheet, FlatList, Image, SafeAreaView,TouchableOpacity } from 'react-native';
+import Constants from 'expo-constants';
+import React, { Component } from 'react'
+
+// You can import from local files
+import AssetExample from './components/AssetExample';
+
+// or any pure javascript modules available in npm
+import { Card } from 'react-native-paper';
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { uri: 'https://images.dog.ceo/breeds/lhasa/n02098413_10590.jpg' }
+  }
+  render() {
+      return (
+        <View style={styles.container}>
+        <Image
+            source={this.state.uri}
+            style={styles.image}
+            />
+          <TouchableOpacity
+          style={{
+                flex: 'row',
+                alignSelf: 'center',
+                }}
+         onPress={this.LoadNewImage}
+        >
+         <Text>Carregar mais imagens</Text>
+        </TouchableOpacity>
+        </View>
+      )
+    }
+}
+
+function LoadNewImage(){
+  console.log("Chamada1")
+  return(
+    getDogsFromApi
+  );
+}
+
+const getDogsFromApi = () => {
+  console.log("Chamada")
+  return fetch('https://dog.ceo/api/breeds/image/random')
+    .then((response) => response.json())
+    .then((json) => {
+        this.setState({ uri: json.message });
+      })
+    .catch((error) => {
+      console.error(error);
+    });
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+  },
+  image: {
+    width: '100%',
+    height: 250,
+    resizeMode:'contain',
+    justifyContent:'center'
+  },
+});
+
+export default App;
